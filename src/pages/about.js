8a39105d235600e8f853ed4b3c534662906d6582
MAtterholt <matterholt84@gmail.com>
2019-03-05 23:03:54 -0500

    update

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Header from "../components/header"
+import Nav from "../components/Nav"
+i
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <Nav />
+    < Header titleName = "About " />   
+  </div>
+)
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "../components/header"
+import Nav from "../components/Nav"
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <Nav />
+    < Header titleName = "Matterholt" />   
+    <p> begining of a new world</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+  </div>
+)
