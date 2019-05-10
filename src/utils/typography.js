3deee243def1d74f8d05fe1663b81a8f486f20a9
MAtterholt <matterholt84@gmail.com>
2019-05-09 22:23:01 -0400

    update typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,12 @@
-import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
+//import Typography from "typography"
+//import Wordpress2016 from "typography-theme-wordpress-2016"
 
+import Typography from "typography"
+import noriegaTheme from "typography-theme-noriega"
+
+const typography = new Typography(noriegaTheme)
+
+/*
 Wordpress2016.overrideThemeStyles = () => {
   return {
     "a.gatsby-resp-image-link": {
@@ -12,7 +18,7 @@ Wordpress2016.overrideThemeStyles = () => {
 delete Wordpress2016.googleFonts
 
 const typography = new Typography(Wordpress2016)
-
+*/
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
   typography.injectStyles()
